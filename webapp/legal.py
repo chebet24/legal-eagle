5c4b5f522f1b1c856392b9fe28d06110e4b8d14c
chebet24 <chebetkitum@gmail.com>
2025-11-03 13:38:33 +0000

    hacker //using vertex ai

--- webapp/legal.py
+++ webapp/legal.py
@@ -3,14 +3,64 @@ import signal
 import sys
 import vertexai
 import random
-from langchain_google_vertexai import VertexAI, VertexAIEmbeddings,VectorSearchVectorStore
-from langchain.chains import RetrievalQA
-from langchain_core.vectorstores import InMemoryVectorStore
-from langchain_core.documents import Document
+from langchain_google_vertexai import VertexAI
 from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
 from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_google_vertexai import VertexAIEmbeddings
+from langchain_google_firestore import FirestoreVectorStore
 
 
+PROJECT_ID = os.environ.get("GOOGLE_CLOUD_PROJECT")  # Get project ID from env
+embedding_model = VertexAIEmbeddings(
+    model_name="text-embedding-004" ,
+    project=PROJECT_ID,)
+
+COLLECTION_NAME = "legal_documents"
+# Create a vector store
+vector_store = FirestoreVectorStore(
+    collection="legal_documents",
+    embedding_service=embedding_model,
+    content_field="original_text",
+    embedding_field="embedding",
+)
+
+def search_resource(query):
+    results = []
+    results = vector_store.similarity_search(query, k=5)
+    
+    combined_results = "\n".join([result.page_content for result in results])
+    print(f"==>{combined_results}")
+    return combined_results
 # Connect to resourse needed from Google Cloud
-llm = VertexAI(model_name="gemini-1.5-flash-002")
+llm = VertexAI(model_name="gemini-2.0-flash")
+def ask_llm(query):
+    try:
+        query_message = {
+            "type": "text",
+            "text": query,
+        }
+        relevant_resource = search_resource(query)
 
+        input_msg = HumanMessage(content=[query_message])
+        prompt_template = ChatPromptTemplate.from_messages(
+            [
+                SystemMessage(
+                    content=(
+                        "You are a helpful assistant, and you are with the attorney in a courtroom, you are helping him to win the case by providing the information he needs "
+                         "Always respond **only in English**, using a confident and energetic tone. "
+                        "Don't answer if you don't know the answer, just say sorry in a funny way possible"
+                        "Use high engergy tone, don't use more than 100 words to answer"
+                       # f"Here is some past conversation history between you and the user {relevant_history}"
+                       # f"Here is some context that is relevant to the question {relevant_resource} that you might use"
+                    )
+                ),
+                input_msg,
+            ]
+        )
+        prompt = prompt_template.format()
+        response = llm.invoke(prompt)
+        print(f"response: {response}")
+        return response
+    except Exception as e:
+        print(f"Error sending message to chatbot: {e}") # Log this error too!
+        return f"Unable to process your request at this time. Due to the following reason: {str(e)}"
--- webapp/main.py
+++ webapp/main.py
@@ -10,6 +10,21 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 @app.route('/',methods=['GET'])
 def index():
     return render_template('index.html')
+
+@app.route('/ask', methods=['POST'])
+def ask_question():
+    data = request.get_json()
+    question = data.get('question')
+    try:
+        # call the ask_llm in legal.py
+        answer_markdown = legal.ask_llm(question)
+        
+        print(f"answer_markdown: {answer_markdown}")
+        # Return the Markdown as the response
+        return answer_markdown, 200
+    except Exception as e:
+        return f"Error: {str(e)}", 500  # Handle errors appropriately
+
     
 # Add this block to start the Flask app when running locally
 if __name__ == "__main__":
